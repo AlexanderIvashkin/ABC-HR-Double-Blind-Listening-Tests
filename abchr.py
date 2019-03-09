dfdd2f19d9dc938be4cfb40e1ac24c289a62b246
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2019-03-09 19:49:13 +0100

    Basically, CSV parsing is working. To be tested.

--- abchr.py
+++ abchr.py
@@ -1,7 +1,25 @@
 import csv
+import subprocess
+import sys
 
-with open('ABC1.csv', newline='') as test_description:
-    csvreader = csv.reader(test_description, delimiter=';', quotechar='"')
-    for z in csvreader:
-        for i in z:
-            print(i)
+
+def main():
+    """Generate ABC/HR files (data is in an CSV)"""
+
+    # check command line for arguments
+    if len(sys.argv) != 2:
+        print ('usage: {:} <CSV with test data>'.format(sys.argv[0]))
+        exit(1)
+
+    # record command line args
+    file_with_test_data = sys.argv[1]
+
+    with open(file_with_test_data, newline='') as test_description:
+        csvreader = csv.DictReader(test_description, delimiter=';', quotechar='"')
+        for z in csvreader:
+            print(z['CmdOriginal'])
+            print(z['CmdHR'])
+            print(z['CmdTested'])
+
+if __name__ == '__main__':
+    main()
